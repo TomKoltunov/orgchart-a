cd0bbf19e6c045fa2096b76009bc50b02f2b4791
TomKoltunov <74180693+TomKoltunov@users.noreply.github.com>
2022-06-11 14:45:50 +0300

    Commit 3

--- Test.cpp
+++ Test.cpp
@@ -1,112 +1,88 @@
-/**
- * @file Test.cpp
- * @author Tom Koltunov
- * @brief Test cases for 5th assignment in Software_Systems_B course
- * @date 2022-05
- */
-
 #include "doctest.h"
 #include "OrgChart.hpp"
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace doctest;
-using namespace std;
+#include "string"
 using namespace ariel;
-
-TEST_CASE("Good input")
+TEST_CASE("Given tree")
 {
-      OrgChart IDF;
-      IDF.add_root("Israeli General Staff") // המטה הכללי
-            .add_sub("Israeli General Staff", "Deputy Chief of Staff") // סגן הרמטכ"ל
-            .add_sub("Israeli General Staff", "Wings") // אגפים
-            .add_sub("Israeli General Staff", "Arms") // זרועות
-            .add_sub("Israeli General Staff", "Commands") // Commands = פיקודים
-            .add_sub("Israeli General Staff", "Military courts") // בתי הדין הצבאיים
-            .add_sub("Israeli General Staff", "Military Rabbinate") // הרבנות הצבאית
-            .add_sub("Israeli General Staff", "Military colleges") // המכללות הצבאיות
+    OrgChart organization;
 
-                  .add_sub("Wings", "Technological and Logistics Directorate") // אגף הטכנולוגיה והלוגיסטיקה
-                  .add_sub("Wings", "Personnel Directorate") // אגף כוח האדם
-                  .add_sub("Wings", "Operations Directorate") // אגף המבצעים
-                  .add_sub("Wings", "Intelligence Directorate") // אגף המודיעין
-                  .add_sub("Wings", "Computer Service Directorate") // אגף התקשוב וההגנה בסייבר
-                  .add_sub("Wings", "Planning Directorate") // אגף התכנון
-
-                        .add_sub("Technological and Logistics Directorate", "Technology and Maintenance Corps") // חיל הטכנולוגיה והאחזקה
-                        .add_sub("Technological and Logistics Directorate", "Medical Corps") // חיל הרפואה 
-                        .add_sub("Technological and Logistics Directorate", "Logistics Corps") // חיל הלוגיסטיקה
-
-                        .add_sub("Personnel Directorate", "Adjutant Corps") // חיל משאבי האנוש
-                        .add_sub("Personnel Directorate", "Military Police Corps") // חיל המשטרה הצבאית
-                        .add_sub("Personnel Directorate", "Education and Youth Corps") // חיל החינוך והנוער
-                        .add_sub("Personnel Directorate", "General Corps") // החיל הכללי
-
-                        .add_sub("Operations Directorate", "Spokesperson's Unit") // דובר צה"ל
-
-                        .add_sub("Intelligence Directorate", "Intelligence Corps") // חיל המודיעין
-
-                        .add_sub("Computer Service Directorate", "C4I Corps") // חיל הקשר והתקשוב
-
-                  .add_sub("Arms", "Ground Forces") // זרוע היבשה
-                  .add_sub("Arms", "Air Force") // זרוע האוויר והחלל
-                  .add_sub("Arms", "Navy") // זרוע הים
-
-                        .add_sub("Ground Forces", "Infantry Corps") // חיל הרגלים
-                        .add_sub("Ground Forces", "Armored Corps") // חיל החימוש
-                        .add_sub("Ground Forces", "Artillery Corps") // חיל התותחנים
-                        .add_sub("Ground Forces", "Combat Engineering Corps") // חיל ההנדסה הקרבית
-                        .add_sub("Ground Forces", "Border Protection Corps") // חיל הגנת הגבולות
-
-                  .add_sub("Commands", "Northern Command") // פיקוד הצפון
-                  .add_sub("Commands", "Central Command") // פיקוד המרכז
-                  .add_sub("Commands", "Southern Command") // פיקוד הדרום
-                  .add_sub("Commands", "Home Front Command") // פיקוד העורף
-                  .add_sub("Commands", "Depth Corps") // מפקדת העומק
-
-                        .add_sub("Northern Command", "Northern Brigade"); // הגיס הצפוני
-
-      vector<string> organization;
-      for (auto it = IDF.begin_level_order(); it != IDF.end_level_order(); ++it)
+    organization.add_root("CEO")
+      .add_sub("CEO", "CTO")         
+      .add_sub("CEO", "CFO")        
+      .add_sub("CEO", "COO")         
+      .add_sub("CTO", "VP_SW") 
+      .add_sub("COO", "VP_BI");
+      std::vector<string> vec;
+      for(auto it=organization.begin_level_order();it!=organization.end_level_order();++it)
       {
-            organization.push_back(*it);
+          vec.push_back(*it);
       }
-      CHECK_EQ(organization.at(0), "Israeli General Staff");
-      CHECK_EQ(organization.at(1), "Deputy Chief of Staff");
-      CHECK_EQ(organization.at(2), "Wings");
-      CHECK_EQ(organization.at(3), "Arms");
-      CHECK_EQ(organization.at(4), "Commands");
-      CHECK_EQ(organization.at(5), "Military courts");
-      CHECK_EQ(organization.at(6), "Military Rabbinate");
-      CHECK_EQ(organization.at(7), "Military colleges");
-      CHECK_EQ(organization.at(8), "Technological and Logistics Directorate");
-      CHECK_EQ(organization.at(9), "Personnel Directorate");
-      CHECK_EQ(organization.at(10), "Operations Directorate");
-      CHECK_EQ(organization.at(11), "Intelligence Directorate");
-      CHECK_EQ(organization.at(12), "Computer Service Directorate");
-      CHECK_EQ(organization.at(13), "Planning Directorate");
-      CHECK_EQ(organization.at(14), "Ground Forces");
-      CHECK_EQ(organization.at(15), "Air Force");
-      CHECK_EQ(organization.at(16), "Navy");
-      CHECK_EQ(organization.at(17), "Northern Command");
-      CHECK_EQ(organization.at(18), "Central Command");
-      CHECK_EQ(organization.at(19), "Southern Command");
-      CHECK_EQ(organization.at(20), "Home Front Command");
-      CHECK_EQ(organization.at(21), "Depth Corps");
-      CHECK_EQ(organization.at(22), "Technology and Maintenance Corps");
-      CHECK_EQ(organization.at(23), "Medical Corps");
-      CHECK_EQ(organization.at(24), "Logistics Corps");
-      CHECK_EQ(organization.at(25), "Adjutant Corps");
-      CHECK_EQ(organization.at(26), "Military Police Corps");
-      CHECK_EQ(organization.at(27), "Education and Youth Corps");
-      CHECK_EQ(organization.at(28), "General Corps");
-      CHECK_EQ(organization.at(29), "Spokesperson's Unit");
-      CHECK_EQ(organization.at(30), "Intelligence Corps");
-      CHECK_EQ(organization.at(31), "C4I Corps");
-      CHECK_EQ(organization.at(32), "Infantry Corps");
-      CHECK_EQ(organization.at(33), "Armored Corps");
-      CHECK_EQ(organization.at(34), "Artillery Corps");
-      CHECK_EQ(organization.at(35), "Combat Engineering Corps");
-      CHECK_EQ(organization.at(36), "Border Protection Corps");
-      CHECK_EQ(organization.at(37), "Northern Brigade");
+      CHECK_EQ(vec.size(),6);
+      CHECK_EQ(vec[0],"CEO");
+      CHECK_EQ(vec[1],"CTO");
+      CHECK_EQ(vec[2],"CFO");
+      CHECK_EQ(vec[3],"COO");
+      CHECK_EQ(vec[4],"VP_SW");   
+      CHECK_EQ(vec[5],"VP_BI");
+      vec.clear();
+      for(auto it:organization)
+      {
+          vec.push_back(it);
+      }
+      CHECK_EQ(vec.size(),6);
+      CHECK_EQ(vec[0],"CEO");
+      CHECK_EQ(vec[1],"CTO");
+      CHECK_EQ(vec[2],"CFO");
+      CHECK_EQ(vec[3],"COO");
+      CHECK_EQ(vec[4],"VP_SW");   
+      CHECK_EQ(vec[5],"VP_BI");
+      vec.clear();
+      for(auto it=organization.begin_reverse_order();it!=organization.reverse_order();++it)
+      {
+          vec.push_back(*it);
+      }
+      CHECK_EQ(vec.size(),6);
+      CHECK_EQ(vec[0],"VP_SW");
+      CHECK_EQ(vec[1],"VP_BI");
+      CHECK_EQ(vec[2],"CTO");
+      CHECK_EQ(vec[3],"CFO");
+      CHECK_EQ(vec[4],"COO");   
+      CHECK_EQ(vec[5],"CEO");
+      vec.clear();
+      for(auto it=organization.begin_preorder();it!=organization.end_preorder();++it)
+      {
+          vec.push_back(*it);
+      }
+      CHECK_EQ(vec.size(),6);
+      CHECK_EQ(vec[0],"CEO");
+      CHECK_EQ(vec[1],"CTO");
+      CHECK_EQ(vec[2],"VP_SW");
+      CHECK_EQ(vec[3],"CFO");
+      CHECK_EQ(vec[4],"COO");   
+      CHECK_EQ(vec[5],"VP_BI");
+}
+TEST_CASE("BAD CASES")
+{
+    OrgChart o;
+
+    std::vector<string> vec;
+    CHECK_THROWS(o.begin());
+    CHECK_THROWS(o.end());
+    CHECK_THROWS(o.begin_level_order());
+    CHECK_THROWS(o.end_level_order());
+    CHECK_THROWS(o.begin_reverse_order());
+    CHECK_THROWS(o.reverse_order());
+    CHECK_THROWS(o.begin_preorder());
+    CHECK_THROWS(o.end_preorder());
+
+    for(int i=0;i<10;i++)
+    {
+        CHECK_THROWS(o.add_sub("SUPER","FAIL"));
+    }
+
+    o.add_root("Parent");
+    for(int i=0;i<10;i++)
+    {
+        CHECK_THROWS(o.add_sub("NOT_EXIST","fail"));
+    }
 }
--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -1,116 +1,470 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <string>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
+const int LEVEL_ORDER = 1;
+const int REVERSE_LEVEL_ORDER = 2;
+const int PREORDER = 3;
+
 namespace ariel
 {
+
+    template <class T = string>
+
     class OrgChart
     {
         private:
             struct Node{
-                string _data;
+                T _data;
                 vector<Node*> _sons;
-                Node(const string &data) : _data(data) 
-                {
 
+                Node(T &data) : _data(data)
+                {
+                    
                 } 
+
+                Node(const Node &n)
+                {
+                    this->_data = n._data;
+                    for (Node *node : n._sons)
+                    {
+                        Node current = Node(*node);
+                        this->_sons.push_back(&current);
+                    }
+                }
+
+                Node(const Node &&n) noexcept
+                {
+                    this->_data = n._data;
+                    this->_sons = n._sons;
+                }
+
+                Node& operator=(Node &&n) noexcept
+                {
+                    return *this;
+                }
+
+                Node& operator=(const Node &n) noexcept
+                {
+                    *this = Node(n);
+                    return *this;
+                }
+                
+                ~Node()
+                {
+                    for (Node *node : this->_sons)
+                    {
+                        node->~Node();
+                    }
+                }
             };
 
             Node *root;
 
         public: 
+
+            OrgChart()
+            {
+                this->root = NULL;
+            }
+
+            OrgChart(OrgChart<T> &org)
+            {
+                if (org.root != NULL)
+                {
+                    this->root = new Node(org.root->_data);
+                    this->root->_sons = org.root->_sons;
+                }
+                else // org.root == NULL 
+                {
+                    this->root = NULL;
+                }
+            }
+
+            OrgChart(OrgChart<T> && org) noexcept
+            {
+                this->root = org.root;
+            }
+
+            ~OrgChart()
+            {
+                if (this->root == NULL)
+                {
+                    return;
+                }
+                this->root->~Node();
+            }
+
+            OrgChart& operator=(OrgChart<T> iter)
+            {
+                if(this != &iter)
+                {
+                    Node *r = this->root;
+                    root = new Node(iter.root->_data);
+                    delete r;
+                }
+                return *this;
+            }
+            
+            OrgChart& operator=(OrgChart<T> &&iter) noexcept
+            {
+                if(this != &iter)
+                {
+                    Node *r = this->root;
+                    root = new Node(iter.root->_data);
+                    delete r;
+                }
+                return *this;
+            }
+
             class iterator
             {
                 private:
-                    string _answer;
+                    vector<Node*> _vec;
+                    int number;
 
                 public:
-                    iterator()
+                    iterator(Node *root, int scan)
                     {
-                        this->_answer = " ";
+                        number = 0;
+                        if (scan == LEVEL_ORDER)
+                        {
+                            level_order(root);
+                        }
+                        else if (scan == REVERSE_LEVEL_ORDER)
+                        {
+                            reverse_level_order(root);
+                        }
+                        else // scan == PREORDER
+                        {
+                            preorder(root);
+                        }
                     }
 
-                    bool operator!=(const iterator& iter) const
+                    bool operator==(const iterator& iter) 
                     {
-                        return false;
+                        bool answer = false;
+                        if ((this->_vec.size() == 0) || (iter._vec.size() == 0))
+                        {
+                           answer = (this->_vec.size() == 0) && (iter._vec.size() == 0);
+                        }
+                        else 
+                        {
+                            answer = (this->_vec[(size_t)number]) == (iter._vec[(size_t)number]);
+                        }
+                        return answer;
+                    }
+
+                    bool operator!=(const iterator& iter)
+                    {
+                        bool answer = !(*this == iter);
+                        return answer;
                     }
 
                     iterator& operator++()
                     {
+                        this->number = this->number + 1;
+                        if (this->number >= this->_vec.size())
+                        {
+                            this->_vec.clear();
+                        }
                         return *this;
                     }
 
-                    iterator operator++(int number)
+                    T& operator*()
                     {
-                        return *this;
+                        T &answer = this->_vec[(size_t)this->number]->_data;
+                        return answer;
                     }
 
-                    string operator*()
+                    T* operator->()
                     {
-                        return " ";
+                        T *answer = &(this->_vec[(size_t)this->number]->_data);
+                        return answer;
                     }
 
-                    string* operator->()
+                    /**
+                    * @brief 
+                    * https://en.wikipedia.org/wiki/Breadth-first_search
+                    * @param root 
+                    */
+                    void level_order(Node *root)
                     {
-                        return &(this->_answer);
+                        queue<Node*> q;
+                        if (root != NULL)
+                        {
+                            q.push(root);
+                        }
+                        Node *current = NULL;
+                        while (!(q.empty()))
+                        {
+                            for (int i = 0; i < q.size(); i++)
+                            {
+                                current = q.front();
+                                q.pop();
+                                this->_vec.push_back(current);
+                                for (int j = 0; j < current->_sons.size(); j++)
+                                {
+                                    q.push(current->_sons[(size_t)j]);
+                                }
+                            }
+                        }
+                    }
+
+                    /**
+                    * @brief 
+                    * https://en.wikipedia.org/wiki/Breadth-first_search
+                    * @param root 
+                    */
+                    void reverse_level_order(Node *root)
+                    {
+                        queue<Node*> q;
+                        if (root != NULL)
+                        {
+                            q.push(root);
+                        }
+                        Node *current = NULL;
+                        while (!(q.empty()))
+                        {
+                            for (int i = 0; i < q.size(); i++)
+                            {
+                                current = q.front();
+                                q.pop();
+                                this->_vec.push_back(current);
+                                for (int j = (current->_sons.size() - 1); j >= 0; j--)
+                                {
+                                    q.push(current->_sons[(size_t)j]);
+                                }
+                            }
+                        }
+                        vector<Node*> v;
+                        for (int i = (this->_vec.size() - 1); i >= 0; i--)
+                        {
+                            v.push_back(this->_vec[(size_t)i]);
+                        }
+                        for (int i = 0; i < v.size(); i++)
+                        {
+                            this->_vec[(size_t)i] = v[(size_t)i];
+                        }
+                        this->_vec = v;
+                    }
+
+                    /**
+                     * @brief 
+                     * https://www.geeksforgeeks.org/iterative-preorder-traversal-of-a-n-ary-tree/
+                     * @param root 
+                     */
+                    void preorder(Node *root)
+                    {
+                        stack<Node*> s;
+                        if (root != NULL)
+                        {
+                            s.push(root);
+                        }
+                        while (!(s.empty()))
+                        {
+                            Node *temp = s.top();
+                            s.pop();
+                            this->_vec.push_back(temp);
+                            for (int i = (temp->_sons.size() - 1); i >= 0; i--)
+                            {
+                                s.push(temp->_sons[(size_t)i]);
+                            }
+                        }
                     }
             };
 
             iterator begin()
             {
-                return iterator();
+                if (this->root != NULL)
+                {
+                    return iterator(this->root, LEVEL_ORDER);
+                }
+                throw invalid_argument{"The tree has no root"};
             }
 
             iterator end()
             {
-                return iterator();
+                if (this->root != NULL)
+                {
+                    return iterator(NULL, LEVEL_ORDER);
+                }
+                throw invalid_argument{"The tree has no root"};
             }
 
             iterator begin_level_order()
             {
-                return iterator();
+                if (this->root != NULL)
+                {
+                    return iterator(this->root, LEVEL_ORDER);
+                }
+                throw invalid_argument{"The tree has no root"};
             }
 
             iterator end_level_order()
             {
-                return iterator();
+                if (this->root != NULL)
+                {
+                    return iterator(NULL, LEVEL_ORDER);
+                }
+                throw invalid_argument{"The tree has no root"};
             }
 
             iterator begin_reverse_order()
             {
-                return iterator();
+                if (this->root != NULL)
+                {
+                    return iterator(this->root, REVERSE_LEVEL_ORDER);
+                }
+                throw invalid_argument{"The tree has no root"};
             }
 
             iterator reverse_order()
             {
-                return iterator();
+                if (this->root != NULL)
+                {
+                    return iterator(NULL, REVERSE_LEVEL_ORDER);
+                }
+                throw invalid_argument{"The tree has no root"};
             }
 
             iterator begin_preorder()
             {
-                return iterator();
+                if (this->root != NULL)
+                {
+                    return iterator(this->root, PREORDER);
+                }
+                throw invalid_argument{"The tree has no root"};
             }
 
             iterator end_preorder()
             {
-                return iterator();
+                if (this->root != NULL)
+                {
+                    return iterator(NULL, PREORDER);
+                }
+                throw invalid_argument{"The tree has no root"};
             }
 
-            OrgChart& add_root(string node)
+            OrgChart& add_root(T data)
             {
+                if ((this->root != NULL) && (data.length() != 0))
+                {
+                    this->root->_data = data;
+                }
+                else // (this->root == NULL) || (data.length() == 0)
+                {
+                    if (data.length() == 0)
+                    {
+                        throw invalid_argument{"There is no node which string is equal to'data'"};
+                    }
+                    this->root = new Node(data); // this->root == NULL
+                }
                 return *this;
             }
 
-            OrgChart& add_sub(string node1, string node2)
+            /**
+             * @brief 
+             * https://codereview.stackexchange.com/questions/39784/search-an-element-item-in-an-n-ary-tree
+             * @param root 
+             * @param data 
+             * @return Node* 
+             */
+            Node* search(Node* root, T data)
             {
-                return *this;
+                queue<Node*> q;
+                Node *found = NULL;
+                Node *current = NULL;
+                if (root != NULL)
+                {
+                    q.push(root);
+                }
+                bool isBreak = false;
+                while (!(q.empty()))
+                {
+                    if (isBreak)
+                    {
+                        break;
+                    }
+                    for (int i = 0; i < q.size(); i++)
+                    {
+                        current = q.front();
+                        q.pop();
+                        for(int j = 0; j < current->_sons.size(); j++)
+                        {
+                            q.push(current->_sons[(size_t)j]);
+                        }
+                        if (current->_data == data)
+                        {
+                            found = current;
+                            isBreak = true;
+                            break;
+                        }
+                    }
+                }
+                return found;
             }
 
-            friend ostream& operator<<(ostream& output, const OrgChart &organization)
+            OrgChart& add_sub(T data1, T data2)
             {
+                Node *some = search(this->root, data1);
+                if ((some != NULL) && (this->root != NULL) && (data1.length() != 0) && (data2.length() != 0))
+                {
+                    some->_sons.push_back(new Node(data2));
+                    return *this;
+                }
+                if (data2.compare(" ") == 0 || data2.compare("\n") == 0 || data2.compare("\t") == 0 || data2.compare("\r") == 0)
+                {
+                    throw invalid_argument{"Sub can't be a space"};
+                }
+                if (data1.length() == 0)
+                {
+                    throw invalid_argument{"There in no node in the organization which string is equal to 'data1'"};
+                }
+                if (data2.length() == 0)
+                {
+                    throw invalid_argument{"There in no node in the organization which string is equal to 'data2'"};
+                }
+                if (some == NULL)
+                {
+                    throw invalid_argument{"There in no node in the organization which string is equal to 'data1'"};
+                }
+                throw invalid_argument{"The tree has no root"}; // this->root == NULL 
+            }
+
+            friend std::ostream& operator<<(std::ostream& output, OrgChart& org){
+
+                queue<Node*>q;
+            
+                q.push(nullptr);
+                q.pop();
+                Node* root=org.root;
+                if(root!=nullptr)
+                {
+                    q.push(root);
+                }
+                while(!q.empty())
+                {
+                    size_t len=q.size();
+                    for(unsigned i=0;i<len;i++)
+                    {
+                        Node* n=q.front();
+                        output<<n->_data<<' '; 
+                        q.pop();
+                        for(size_t j=0;j<n->_sons.size();j++)
+                        {
+                            q.push(n->_sons.at(j));
+                        }
+                    }
+                    output<<'\n';
+                }
                 return output;
             }
-    };
+
+        };
 }
